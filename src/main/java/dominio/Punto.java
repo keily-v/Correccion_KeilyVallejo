fcd625d600a55330b09ec9e1014aa085d852ed56
Keily Vallejo <kmvallejo2@espe.edu.ec>
2021-06-29 16:45:15 -0500

    v0

---
+++ src/main/java/dominio/Punto.java
@@ -0,0 +1,57 @@
+package dominio;
+
+public class Punto {
+    private int x;
+    private int y;
+    private double calcularDistancia;
+
+    public Punto(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    public Punto(double calcularDistancia) {
+        this.calcularDistancia = calcularDistancia;
+    }
+    
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double getCalcularDistancia() {
+        return calcularDistancia;
+    }
+    public void setCalcularDistancia(Punto P2) {
+        
+        int x2=P2.getX();
+        int y2=P2.getY();
+        int x1=this.x;
+        int y1=this.y;
+        int resta1,resta2;
+        resta1=x2-x1;
+        System.out.println("resta1 :"+resta1);
+        resta2=y2-y1;
+        System.out.println("resta 2:"+resta2);
+        double c1 = Math.pow(resta1,2);
+        double c2 = Math.pow(resta2,2);
+        this.calcularDistancia = Math.sqrt(c1+c2);
+    }
+
+
+    @Override
+    public String toString(){
+        return "P("+this.x+","+this.y+")";
+    }
+}
---
+++ src/main/java/test/PruebaPunto.java
@@ -0,0 +1,63 @@
+package test;
+
+import dominio.Punto;
+import java.util.Scanner;
+
+public class PruebaPunto {
+
+    static Scanner datos = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("PROGRAMA QUE CALCULA LA DISTANCIA DE UN PUNTO "
+                + "CON MOVIMIENTOS ALEATORIOS EN UN PLANO CARTESIANO ");
+
+        System.out.println("¿Cuántos movimientos desea que realice el punto?");
+        int n = datos.nextInt();
+        Punto[] puntos = new Punto[n];
+        generarPuntos(puntos);
+        System.out.println("Los puntos son: ");
+        for (Punto p : puntos) {
+            System.out.println(p);
+        }
+
+    }
+
+    public static void generarPuntos(Punto puntos[]) {
+
+        int x=0, y=0;
+
+        for (int i = 0; i < puntos.length; i++) {
+            int mover = ((int) (Math.random() * 4 + 1));
+
+            System.out.println("¿Qué cantidad desea que se mueva el punto en el movimiento : " +(i + 1) + "?");
+            int cantidadmovimientos = datos.nextInt();
+            if (mover == 1) {
+                y = y + cantidadmovimientos;
+                System.out.println("El punto se movió hacia arriba");
+                System.out.println("x: " + x + ",y: " + y);
+            } else if (mover == 2) {
+                y = y - cantidadmovimientos;
+                System.out.println("El punto se movió hacia abajo");
+                System.out.println("x: " + x + ",y: " + y);
+            } else if (mover == 3) {
+                x = x + cantidadmovimientos;
+                System.out.println("El punto se movió hacia la derecha");
+                System.out.println("x: " + x + ",y: " + y);
+            } else if (mover == 4) {
+                x = x - cantidadmovimientos;
+                System.out.println("El punto se movió hacia la izquierda");
+                System.out.println("x: " + x + ",y: " + y);
+            } else {
+                System.out.println("ERROR");
+            }
+            puntos[i] = new Punto(x, y);
+            
+            for (int j = 0; j < 1; j++) {
+                puntos[i].setCalcularDistancia(puntos[j]);
+                System.out.println("La distancia es:" + puntos[i].getCalcularDistancia());
+            }
+
+        }
+    }
+}
